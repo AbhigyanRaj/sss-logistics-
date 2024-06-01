5d1ef6a419a242432ea098fe4076c4602939012f
ABHIGYAN <155881024+AbhigyanRaj@users.noreply.github.com>
2024-06-01 19:59:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -128,3 +128,21 @@ AOS.init({
     easing: "ease",
     once: true, // whether animation should happen only once - while scrolling down
 });
+
+
+
+
+// custom cursor 
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 200);
+});
